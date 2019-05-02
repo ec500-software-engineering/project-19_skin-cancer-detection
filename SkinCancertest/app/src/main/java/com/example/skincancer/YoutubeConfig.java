5117a1989a43e162d1af8b9515e3da44f858e3cc
vansoncc <43052978+vansoncc@users.noreply.github.com>
2019-05-02 19:25:21 -0400

    Update YoutubeConfig.java

--- SkinCancertest/app/src/main/java/com/example/skincancer/YoutubeConfig.java
+++ SkinCancertest/app/src/main/java/com/example/skincancer/YoutubeConfig.java
@@ -4,7 +4,7 @@ public class YoutubeConfig {
     public YoutubeConfig(){
 
     }
-    private  static  final  String API_key = "AIzaSyC6kxNZaTvJNpW-ay6aqA6AwB6rrcqaL-w";
+    private  static  final  String API_key = "--w";
 
     public static String getAPI_key() {
         return API_key;
